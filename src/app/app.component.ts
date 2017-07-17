a56c7172c556be848a4faaadf571980cfd7048df
johnny99tech <jonathan99moura@hotmail.com>
2017-07-17 10:35:25 -0300

    Qualquer coisa é só se desesperar...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,35 +15,37 @@ import {SamuService} from './services/samu.service'
 export class AppComponent implements OnInit {
     ufs : UF[];
     dados_da_samu : Dados[];
-    brasil_uf: UF;
-    media: number = 0;
+    minha_uf: UF;
     municipios_atendidos: Dados[] = [];
-    estado_id = 25;
-    
+    meu_id = 25;
+    media: number = 0;
+
     constructor(private ufService: UFService, private samuService: SamuService)
     { }
 
     ngOnInit(): void {
         this.ufs = this.ufService.getAll();
         this.dados_da_samu = this.samuService.getAllMunicipiosAtendidosPorEstado();
-        this.brasil_uf = this.UF();
+        this.minha_uf = this.UF();
         this.media = this.calcular();
     }
+
     UF(): UF {
       for (let uf of this.ufs) {
-          if (uf.id == this.estado_id) return uf;
+          if (uf.id == this.meu_id) return uf;
       }
     }
+
     calcular(): number {
-      var anos = 0;
-      var tudo = 0;
+      var qnt = 0;
+      var total = 0;
       for (let mun of this.dados_da_samu){
-        if (mun.uf_id == this.estado_id){
-          anos++;
-          tudo+=mun.valor;
+        if (mun.uf_id == this.meu_id){
+          qnt++;
+          total+=mun.valor;
           this.municipios_atendidos.push(mun);
         }
       }
-        return Math.round(tudo/anos);
+        return Math.round(total/qnt);
     }
 }
